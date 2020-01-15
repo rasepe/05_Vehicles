eccc3f87b0f89b6b6ce8a0f0af7f48f5c62a0255
Rafel <rasepe@gmail.com>
2020-01-15 12:48:47 +0100

    Fase 3 - 150120

--- VehiclesProject-master/VehiclesProject/src/com/vehicles/project/Main.java
+++ VehiclesProject-master/VehiclesProject/src/com/vehicles/project/Main.java
@@ -1,5 +1,6 @@
 package com.vehicles.project;
 
+
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
@@ -7,15 +8,42 @@ import java.util.Scanner;
 
 public class Main {
 
-	public static void main(String[] args) { // throws VehiclesException  {
+	public static void main(String[] args) { 
+		menu();
+	}
+
+	public static void menu() {
+		Scanner sc = new Scanner(System.in);
+		int option;
+
+		System.out.println("Escull una opció:");
+		System.out.println("1. Crear cotxe");
+		System.out.println("2. Crear moto");
+		System.out.println("3. Sortir");
+		option = sc.nextInt();
+		switch(option) {
+		case 1:
+			addCar();
+			break;
+		case 2:
+			addBike();
+			break;
+		default:
+			System.out.println("FI");
+			sc.close();
+		}	
+	}
+
+	public static void addCar() {		
 		Scanner sc = new Scanner(System.in);
 
 		String plate;
 		String brand;
 		String color;
+
 		Car c1;
 
-		boolean correcte = false;
+		boolean correct = false;
 
 
 		//Pregunta matricula amb control errors:
@@ -45,18 +73,19 @@ public class Main {
 					if (Character.getNumericValue(characters[k]) > -1 && Character.getNumericValue(characters[k]) < 10) {
 						throw new Exception();
 					} else {
-						correcte = true;
+						correct = true;
 					}
 				}		
 			} catch (Exception e) {
 				System.out.println(VehiclesException.WRONG_PLATE);
 			}  
-		} while (correcte == false);
+		} while (correct == false);
 
 		System.out.println("Marca?");
 		brand = sc.next();
 		System.out.println("Color?");
 		color = sc.next();
+
 
 		c1 = new Car(plate, brand, color);
 
@@ -73,7 +102,7 @@ public class Main {
 		brandBackWheels = sc.next();
 
 		//pregunta diametre traseres amb control errors
-		correcte = false;
+		correct = false;
 		do {
 			System.out.println("Diàmetre rodes traseres?");
 			diameterBackWheels = sc.nextDouble();
@@ -81,12 +110,12 @@ public class Main {
 				if (diameterBackWheels<=0.4 || diameterBackWheels >=4) {
 					throw new Exception(); 
 				} else {
-					correcte = true;
+					correct = true;
 				}
 			}catch (Exception e) {
 				System.out.println(VehiclesException.WRONG_DIAMETER);
 			}  
-		} while (correcte == false);
+		} while (correct == false);
 
 		rightBackWheel = new Wheel(brandBackWheels, diameterBackWheels);
 		leftBackWheel = new Wheel(brandBackWheels, diameterBackWheels); 
@@ -95,23 +124,23 @@ public class Main {
 		brandFrontWheels = sc.next();
 
 		//pregunta diametre davanteres amb control errors
-		correcte = false;
+		correct = false;
 		do {
 			System.out.println("Diàmetre rodes davanteres?");
 			diameterFrontWheels = sc.nextDouble();
 			try {
 				if (diameterFrontWheels<=0.4 || diameterFrontWheels >=4) {
-					throw new Exception(); // VehiclesException(VehiclesException.WRONG_DIAMETER);
+					throw new Exception();
 				} else {
-					correcte = true;
+					correct = true;
 				}
 			}catch (Exception e) {
 				System.out.println(VehiclesException.WRONG_DIAMETER);
 			}  
-		} while (correcte == false);
+		} while (correct == false);
 
 		rightFrontWheel = new Wheel(brandFrontWheels, diameterFrontWheels);
-		leftFrontWheel = new Wheel(brandFrontWheels, diameterFrontWheels); //= rightFrontWheel;
+		leftFrontWheel = new Wheel(brandFrontWheels, diameterFrontWheels); 
 
 		List<Wheel> frontWheels = new ArrayList<Wheel>(); 
 		frontWheels.add(rightFrontWheel);
@@ -128,8 +157,132 @@ public class Main {
 			e.printStackTrace();
 		}
 
-		System.out.println("FI");
 		sc.close();
 
+		System.out.println("COTXE CREAT CORRECTAMENT");
+
+		menu();
+
 	}
+
+
+	public static void addBike() {		
+		Scanner sc = new Scanner(System.in);
+
+		String plate;
+		String brand;
+		String color;
+
+		Bike b1;
+
+		boolean correct = false;
+
+		//Pregunta matricula amb control errors:
+		do {
+			//Una matrícula ha de tenir 4 números i dues o tres lletres.
+			System.out.println("Matrícula?");
+			plate = sc.next();
+			try {
+				//llargada minima 6, llargada maxima 7
+				if (!(plate.length() > 5 && plate.length() < 8 )) {
+					throw new Exception();
+				}
+
+				//4 primers caracters han de ser numerics
+				char[]characters = new char[plate.length()];
+				for (int i = 0; i < characters.length ; i++) {
+					characters[i]=plate.charAt(i);
+				}
+				for (int j = 0; j < 4 ; j++) {
+					if (Character.getNumericValue(characters[j]) < 0 || Character.getNumericValue(characters[j]) > 9) {
+						throw new Exception();
+					} 
+				}
+
+				//2 o 3 ultims caracters han de ser lletres (= no han de ser numeros)
+				for (int k = 4; k < characters.length ; k++) {
+					if (Character.getNumericValue(characters[k]) > -1 && Character.getNumericValue(characters[k]) < 10) {
+						throw new Exception();
+					} else {
+						correct = true;
+					}
+				}		
+			} catch (Exception e) {
+				System.out.println(VehiclesException.WRONG_PLATE);
+			}  
+		} while (correct == false);
+
+		System.out.println("Marca?");
+		brand = sc.next();
+		System.out.println("Color?");
+		color = sc.next();
+
+		b1 = new Bike(plate, brand, color);
+
+		String brandBackWheel;
+		double diameterBackWheel;
+		String brandFrontWheel;
+		double diameterFrontWheel;
+		Wheel backWheel;
+		Wheel frontWheel;
+
+		System.out.println("Marca roda trasera?");
+		brandBackWheel = sc.next();
+
+
+		//pregunta diametre roda trasera amb control errors
+		correct = false;
+		do {
+			System.out.println("Diàmetre roda trasera?");
+			diameterBackWheel = sc.nextDouble();
+			try {
+				if (diameterBackWheel<=0.4 || diameterBackWheel >=4) {
+					throw new Exception(); 
+				} else {
+					correct = true;
+				}
+			}catch (Exception e) {
+				System.out.println(VehiclesException.WRONG_DIAMETER);
+			}  
+		} while (correct == false);
+
+		backWheel = new Wheel(brandBackWheel, diameterBackWheel);
+
+		System.out.println("Marca roda davantera?");
+		brandFrontWheel = sc.next();
+
+		//pregunta diametre roda davantera amb control errors
+		correct = false;
+		do {
+			System.out.println("Diàmetre roda davantera?");
+			diameterFrontWheel = sc.nextDouble();
+			try {
+				if (diameterFrontWheel<=0.4 || diameterFrontWheel >=4) {
+					throw new Exception(); 
+				} else {
+					correct = true;
+				}
+			}catch (Exception e) {
+				System.out.println(VehiclesException.WRONG_DIAMETER);
+			}  
+		} while (correct == false);
+
+		frontWheel = new Wheel(brandFrontWheel, diameterFrontWheel);
+
+
+		try {
+			b1.addWheels(frontWheel, backWheel);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		sc.close();
+
+		System.out.println("MOTO CREADA CORRECTAMENT");
+
+		menu();
+
+	}
+
 }
--- VehiclesProject-master/VehiclesProject/src/com/vehicles/project/Wheel.java
+++ VehiclesProject-master/VehiclesProject/src/com/vehicles/project/Wheel.java
@@ -10,17 +10,10 @@ public class Wheel {
 		this.brand = brand;
 		this.diameter = diameter;
 	}
-	/*
-	 * public double getDiameter() { return diameter; }
-	 * 
-	 * public void setDiameter(double diameter) { this.diameter = diameter; }
-	 */
+
 	
 	public boolean equals(Object o) {
-		/*
-		 * if (this == o) { return true; } if (o == null || getClass() != o.getClass())
-		 * { return false; }
-		 */
+
         Wheel wheel = (Wheel) o;
         return this.brand.equals(wheel.brand) &&
         		this.diameter == wheel.diameter;
